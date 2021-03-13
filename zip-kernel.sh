7913a486929aa55905a42810d00713992d2dae36
Ahad Kabir Siddique <ahadkabirsiddique@gmail.com>
2021-03-13 15:41:08 +0600

    add zip script

---
+++ zip-kernel.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+echo "Copying kernel image"
+cp /home/ahadrox/Desktop/android-kernel/lavender/out/arch/arm64/boot/Image.gz-dtb ./
+
+echo "Zipping kernel"
+
+zip -r9 UPDATE-Kernel.zip * -x .git README.md *placeholder
+
+rm -rf Image.gz-dtb
+
+echo "Done!"
